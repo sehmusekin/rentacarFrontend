eb139b8e799054743898e65aa2b459acf9f05003
Taner Dökmetaş <tanerdokmets@gmail.com>
2024-01-17 12:50:36 +0300

    Footer Added

--- rentacarFrontend/src/App.tsx
+++ rentacarFrontend/src/App.tsx
@@ -5,6 +5,7 @@ import Locations from "./pages/Locations/Locations";
 import Review from "./pages/Review/Review";
 import Cars from "./pages/Cars/Cars";
 import CarDetail from "./pages/CarsDetail/CarDetail";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/cars" element={<Cars />}></Route>
         <Route path="/car-detail/:id" element={<CarDetail />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
