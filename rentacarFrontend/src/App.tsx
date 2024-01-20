eea2232086826862650ca8256b263f7911b9a425
Taner Dökmetaş <tanerdokmets@gmail.com>
2024-01-20 16:18:42 +0300

    New design new excitement

--- rentacarFrontend/src/App.tsx
+++ rentacarFrontend/src/App.tsx
@@ -1,23 +1,5 @@
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-
-import Locations from "./pages/Locations/Locations";
-import Review from "./pages/Review/Review";
-import Cars from "./pages/Cars/Cars";
-import CarDetail from "./pages/CarsDetail/CarDetail";
-
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/locations" element={<Locations />}></Route>
-        <Route path="/review" element={<Review />}></Route>
-        <Route path="/cars" element={<Cars />}></Route>
-        <Route path="/car-detail/:id" element={<CarDetail />}></Route>
-      </Routes>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
