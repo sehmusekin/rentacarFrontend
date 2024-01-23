08edfb0846cc8d12ec66045be8c58c57a029a178
t4ner <tanerdokmets@gmail.com>
2024-01-23 23:47:27 +0300

    Footer responsive added

--- rentacarFrontend/src/components/Footer.tsx
+++ rentacarFrontend/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
     <div className="bg-white p-6 w-full">
-      <div className="w-3/4 mx-auto flex justify-ar">
-        <div className=" basis-1/4">
+      <div className="w-full md:w-3/4 mx-auto flex-col lg:flex lg:flex-row">
+        <div className=" md:basis-1/4">
           <h4 className="text-blue-900 font-extrabold text-3xl">Rent-A-Car</h4>
           <p className="text-gray-500 mt-4">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore,
@@ -10,10 +10,10 @@ function Footer() {
             voluptatem.
           </p>
         </div>
-        <div className="flex basis-3/4 justify-end gap-12">
+        <div className="flex md:basis-3/4 justify-around lg:justify-end gap-6 lg:gap-12 mt-6 lg:mt-0">
           <div>
             <h4 className="font-bold text-xl">About</h4>
-            <ul className="space-y-3 text-gray-500 mt-4">
+            <ul className="space-y-3 text-gray-500 mt-4 text-sm md:text-base">
               <li>How it works</li>
               <li>Featured</li>
               <li>Partnership</li>
@@ -22,7 +22,7 @@ function Footer() {
           </div>
           <div>
             <h4 className="font-bold text-xl">Community</h4>
-            <ul className="space-y-3 text-gray-500 mt-4">
+            <ul className="space-y-3 text-gray-500 mt-4 text-sm md:text-base">
               <li>Events</li>
               <li>Blog</li>
               <li>Podcast</li>
@@ -31,7 +31,7 @@ function Footer() {
           </div>
           <div>
             <h4 className="font-bold text-xl">Socials</h4>
-            <ul className="space-y-3 text-gray-500 mt-4">
+            <ul className="space-y-3 text-gray-500 mt-4 text-sm md:text-base">
               <li>Discord</li>
               <li>Instagram</li>
               <li>Twitter</li>
