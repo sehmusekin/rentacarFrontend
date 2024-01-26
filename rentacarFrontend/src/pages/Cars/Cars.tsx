b6665cd530e440b81ecb2d80603c4e810540bfc3
Taner <tanerdokmets@gmail.com>
2024-01-26 22:11:08 +0300

    MoreCars functions  added

--- rentacarFrontend/src/pages/Cars/Cars.tsx
+++ rentacarFrontend/src/pages/Cars/Cars.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import CarList from "./components/CarList";
 import FilterSidebar from "./components/FilterSidebar";
 import { Car } from "../../models/responses/car/GetAllCarResponses";
@@ -6,20 +6,45 @@ import CarService from "../../services/CarService/CarService";
 
 function Cars() {
   const [cars, setCars] = useState<Car[]>([]);
+  const [showAllCars, setShowAllCars] = useState(false);
 
   useEffect(() => {
-    let carService = new CarService();
-    carService.getAllCars().then((result) => {
-      setCars(result.data.data);
-    });
+    const fetchCars = async () => {
+      try {
+        const carService = new CarService();
+        const result = await carService.getAllCars();
+        setCars(result.data.data);
+      } catch (error) {
+        console.error("Error fetching cars:", error);
+      }
+    };
+
+    fetchCars();
   }, []);
+
+  const handleShowAllCars = () => {
+    setShowAllCars(true);
+  };
+
   return (
-    <div className="flex gap-4 p-5">
-      <FilterSidebar />
-      <div className="basis-3/4 flex flex-wrap">
-        {cars.map((car, index) => (
-          <CarList car={car} key={index} />
-        ))}
+    <div className="flex flex-col">
+      <div className="flex gap-4 p-5">
+        <FilterSidebar />
+        <div className="basis-3/4 flex flex-wrap">
+          {cars.slice(0, showAllCars ? cars.length : 9).map((car, index) => (
+            <CarList car={car} key={index} />
+          ))}
+        </div>
+      </div>
+      <div className="h-full ml-auto mb-10">
+        {!showAllCars && cars.length > 10 && (
+          <button
+            className="text-blue-600 underline hover:no-underline transition duration-700 font-bold mr-5"
+            onClick={handleShowAllCars}
+          >
+            MORE CAR
+          </button>
+        )}
       </div>
     </div>
   );
