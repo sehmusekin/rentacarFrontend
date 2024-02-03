0c65792f9d9225765a916fdc1433279d0bde60fe
Taner <tanerdokmets@gmail.com>
2024-02-03 16:32:38 +0300

    Rental page updated

--- rentacarFrontend/src/pages/Rental/Rental.tsx
+++ rentacarFrontend/src/pages/Rental/Rental.tsx
@@ -1,7 +1,159 @@
 function Rental() {
   return (
     <div className="flex gap-10 my-10 justify-center">
-     
+      <div>
+        <div className="bg-white w-[631px] h-[360px] p-6 rounded-lg">
+          <h4 className="text-lg font-bold">Billing Info</h4>
+          <p className="text-gray-400 mt-1 mb-5">
+            Please enter your billing info
+          </p>
+          <div>
+            <div className="flex">
+              <div className="flex flex-col">
+                <label className="font-semibold" htmlFor="">
+                  Name
+                </label>
+                <input
+                  className="border p-2 rounded-lg w-[272px] h-[48px]"
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Name"
+                />
+              </div>
+              <div className="flex flex-col ml-10">
+                <label className="font-semibold" htmlFor="">
+                  Adress
+                </label>
+                <input
+                  className="border p-2 rounded-lg w-[272px] h-[48px]"
+                  type="text"
+                  placeholder="Adress"
+                />
+              </div>
+            </div>
+            <div className="flex mt-10">
+              <div className="flex flex-col">
+                <label className="font-semibold" htmlFor="">
+                  Phone
+                </label>
+                <input
+                  className="border p-2 rounded-lg w-[272px] h-[48px]"
+                  type="text"
+                  placeholder="Phone"
+                  name=""
+                  id=""
+                />
+              </div>
+              <div className="flex flex-col ml-10">
+                <label className="font-semibold" htmlFor="">
+                  Town/City
+                </label>
+                <input
+                  className="border p-2 rounded-lg w-[272px] h-[48px]"
+                  type="text"
+                  placeholder="Town/City"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white w-[631px]  p-6 rounded-lg mt-10">
+          <h4 className="text-lg font-bold">Rental Info</h4>
+          <p className="text-gray-400">Please enter your billing info</p>
+          <p className="font-semibold my-6">Pick-Up</p>
+          <hr className="my-6" />
+          <div className="flex flex-wrap justify-between">
+            <div className="flex flex-col">
+              <label className="font-semibold" htmlFor="">
+                Locations
+              </label>
+              <input
+                className="border w-[272px] h-[48px] p-2 rounded-lg"
+                type="text"
+                placeholder="Locations"
+              />
+            </div>
+            <div className="flex flex-col ">
+              <label className="font-semibold" htmlFor="">
+                Date
+              </label>
+              <input
+                className="border w-[272px] h-[48px] p-2 rounded-lg"
+                type="datetime-local"
+                placeholder="Date"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="font-semibold" htmlFor="">
+                Time
+              </label>
+              <input
+                className="border w-[272px] h-[48px] p-2 rounded-lg"
+                type="time"
+                placeholder="Time"
+              />
+            </div>
+          </div>
+          <p className="font-semibold my-6">Drop-Off</p>
+          <hr className="my-6" />
+          <div className="flex flex-wrap justify-between">
+            <div className="flex flex-col">
+              <label className="font-semibold" htmlFor="">
+                Locations
+              </label>
+              <input
+                className="border w-[272px] h-[48px] p-2 rounded-lg"
+                type="text"
+                placeholder="Locations"
+              />
+            </div>
+            <div className="flex flex-col ">
+              <label className="font-semibold" htmlFor="">
+                Date
+              </label>
+              <input
+                className="border w-[272px] h-[48px] p-2 rounded-lg"
+                type="datetime-local"
+                placeholder="Date"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="font-semibold" htmlFor="">
+                Time
+              </label>
+              <input
+                className="border w-[272px] h-[48px] p-2 rounded-lg"
+                type="time"
+                placeholder="Time"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-white w-[631px] h-[350px] p-6 rounded-lg mt-10">
+          <h4 className="text-lg font-bold">Confirmation</h4> 
+          <p className="text-gray-400">
+            We are getting to the end. Just few clicks and your rental is ready!
+          </p>
+          <div className="flex gap-5 items-center bg-gray-200 p-4 rounded-lg my-5">
+            <input type="checkbox" />
+            <p className="font-semibold">
+              I agree with sending an Marketing and newsletter emails. No spam,
+              promissed!
+            </p>
+          </div>
+          <div className="flex gap-5 items-center bg-gray-200 p-4 rounded-lg">
+            <input type="checkbox" />
+            <p className="font-semibold">
+              I agree with our terms and conditions and privacy policy!
+            </p>
+          </div>
+          <button className="border py-2 px-4 bg-blue-600 rounded-lg text-white font-semibold mt-5">
+            Rental Now
+          </button>
+        </div>
+      </div>
       <div>
         <div className="bg-white w-[631px] h-[600px] p-6 rounded-lg">
           <h4 className="text-lg font-bold">Rental Summary</h4>
