3f73acfef1791c3dcd662650b9b0de61ba3da091
Taner <tanerdokmets@gmail.com>
2024-02-15 14:13:46 +0300

    FilterSidebar component updated

--- rentacarFrontend/src/pages/Cars/components/FilterSidebar.tsx
+++ rentacarFrontend/src/pages/Cars/components/FilterSidebar.tsx
@@ -20,62 +20,87 @@ export default function FilterSidebar() {
   const handleSliderChange = (event: any) => {
     setSliderValue(event.target.value);
   };
-
+  console.log(category);
   return (
-    <>
-      {category.map((item: Category) => (
-        <div key={item.id} className="basis-1/4  text-gray-500 ">
-          <div className="basis-1/4  text-gray-500 ">
-            <div className="flex flex-wrap md:flex md:flex-col md:flex-nowrap md:space-y-10 bg-white p-5 rounded-lg">
-              <div className="basis-1/2 flex flex-col space-y-2">
-                <h4 className="text-xs text-gray-400 tracking-wider">TYPE</h4>
-                <label>
-                  <input className="mr-2" type="checkbox" value="audi" />
-                  {item.name}
-                </label>
-              </div>
-              <div className="basis-1/2 flex flex-col space-y-2">
-                <h4 className="text-xs text-gray-400 tracking-wider">
-                  CAPACİTY
-                </h4>
-                <label>
-                  <input className="mr-2" type="checkbox" value="audi" />2
-                  Person
-                </label>
-              </div>
-              <div className="flex basis-1/2 flex-col space-y-2 mt-5 md:mt-0">
-                <h4 className="text-xs text-gray-400 tracking-wider">COLOR</h4>
-                <label>
-                  <input className="mr-2" type="checkbox" />
-                  White
-                </label>
-              </div>
-              <div className="flex basis-1/2 flex-col space-y-2 mt-5 md:mt-0">
-                <h3 className="text-xs text-gray-400 tracking-wider">FUEL</h3>
-                <label>
-                  <input className="mr-2" type="checkbox" />
-                  Gasoline
-                </label>
-              </div>
-              <div className="flex flex-col mt-5 md:mt-5">
-                <h3 className=" text-xs text-gray-400 tracking-wider">PRICE</h3>
-
-                <label className=" flex flex-col">
-                  <input
-                    type="range"
-                    min="90"
-                    max="600"
-                    value={sliderValue}
-                    onChange={handleSliderChange}
-                    className="my-2"
-                  />
-                  <p className="text-sm">PRICE {sliderValue}$</p>
-                </label>
-              </div>
+    <div className="basis-1/4  text-gray-500 ">
+      <div className="flex flex-wrap md:flex md:flex-col md:flex-nowrap md:space-y-10 bg-white p-5 rounded-lg">
+        <div className="basis-1/2 flex flex-col space-y-2">
+          <h4 className="text-xs text-gray-400 tracking-wider">TYPE</h4>
+          {category.map((category: Category) => (
+            <div key={category.id} className="mb-4">
+              <label className="block">
+                <input className="mr-2" type="checkbox" value={category.name} />
+                {category.name}
+              </label>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
-    </>
+        <div className="basis-1/2 flex flex-col space-y-2">
+          <h4 className="text-xs text-gray-400 tracking-wider">CAPACİTY</h4>
+          <label>
+            <input className="mr-2" type="checkbox" value="audi" />2 Person
+          </label>
+          <label>
+            <input className="mr-2" type="checkbox" value="audi" />4 Person
+          </label>
+          <label>
+            <input className="mr-2" type="checkbox" value="audi" />6 Person
+          </label>
+        </div>
+        <div className="flex basis-1/2 flex-col space-y-2 mt-5 md:mt-0">
+          <h4 className="text-xs text-gray-400 tracking-wider">COLOR</h4>
+          <label>
+            <input className="mr-2" type="checkbox" />
+            White
+          </label>
+          <label>
+            <input className="mr-2" type="checkbox" />
+            Black
+          </label>
+          <label>
+            <input className="mr-2" type="checkbox" />
+            Red
+          </label>
+          <label>
+            <input className="mr-2" type="checkbox" />
+            Gray
+          </label>
+        </div>
+        <div className="flex basis-1/2 flex-col space-y-2 mt-5 md:mt-0">
+          <h3 className="text-xs text-gray-400 tracking-wider">FUEL</h3>
+          <label>
+            <input className="mr-2" type="checkbox" />
+            Gasoline
+          </label>
+          <label>
+            <input className="mr-2" type="checkbox" />
+            Diesel
+          </label>
+          <label>
+            <input className="mr-2" type="checkbox" />
+            LPG
+          </label>
+          <label>
+            <input className="mr-2" type="checkbox" />
+            Electricity
+          </label>
+        </div>
+        <div className="flex flex-col mt-5 md:mt-5">
+          <h3 className=" text-xs text-gray-400 tracking-wider">PRICE</h3>
+
+          <label className=" flex flex-col">
+            <input
+              type="range"
+              min="90"
+              max="600"
+              value={sliderValue}
+              onChange={handleSliderChange}
+              className="my-2"
+            />
+            <p className="text-sm">PRICE {sliderValue}$</p>
+          </label>
+        </div>
+      </div>
+    </div>
   );
 }
