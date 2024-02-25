49e6b432ab38b4d3c1f21a1ed1fc45f598a6eda0
Taner <tanerdokmets@gmail.com>
2024-02-25 14:56:07 +0300

    ColorList and FuelTypeList updated

--- rentacarFrontend/src/pages/FuelTypeList/FuelTypeList.tsx
+++ rentacarFrontend/src/pages/FuelTypeList/FuelTypeList.tsx
@@ -10,6 +10,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { Car } from "../../models/responses/car/GetAllCarResponses";
+import FilterSidebar from "../Cars/components/FilterSidebar";
 
 export default function CategoryList() {
   const { id } = useParams();
@@ -29,58 +30,64 @@ export default function CategoryList() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        
-      {cars.map((car) => (
-        <div
-          key={car.id}
-          className="border bg-white w-full md:w-auto mx-auto md:mx-0 p-5 rounded-lg space-y-8"
-        >
-          <div>
-            <h4 className="font-bold">{car.brandName}</h4>
-            <h4 className="font-bold">{car.modelName}</h4>
-            <p className="text-gray-400">{car.categoryName}</p>
-          </div>
-          <div>
-            <img src={heroimage2} width={280} alt="" />
-          </div>
-          <div className="flex justify-around">
-            <div className="flex items-center">
-              <FontAwesomeIcon
-                className="mr-1 text-gray-400"
-                icon={faGasPump}
-              />
-              <p className="text-sm text-gray-400">{car.fuelType}</p>
+    <div className="flex">
+      <div className="basis-1/3 m-5">
+        <FilterSidebar />
+      </div>
+      <div className="flex flex-wrap p-5 mx-auto">
+        {cars.map((car) => (
+          <div
+            key={car.id}
+            className="border bg-white w-full md:w-auto mx-auto md:mx-0 p-5 rounded-lg space-y-8"
+          >
+            <div>
+              <h4 className="font-bold">{car.brandName}</h4>
+              <h4 className="font-bold">{car.modelName}</h4>
+              <p className="text-gray-400">{car.categoryName}</p>
             </div>
-            <div className="flex items-center">
-              <FontAwesomeIcon
-                className="mr-1 text-gray-400"
-                icon={faDroplet}
-              />
-              <p className="text-sm text-gray-400">{car.colorName}</p>
+            <div>
+              <img src={heroimage2} width={280} alt="" />
             </div>
-            <div className="flex items-center">
-              <FontAwesomeIcon
-                className="mr-1 text-gray-400"
-                icon={faCalendarDays}
-              />
-              <p className="text-sm text-gray-400">{car.year}</p>
+            <div className="flex justify-around">
+              <div className="flex items-center">
+                <FontAwesomeIcon
+                  className="mr-1 text-gray-400"
+                  icon={faGasPump}
+                />
+                <p className="text-sm text-gray-400">{car.fuelType}</p>
+              </div>
+              <div className="flex items-center">
+                <FontAwesomeIcon
+                  className="mr-1 text-gray-400"
+                  icon={faDroplet}
+                />
+                <p className="text-sm text-gray-400">{car.colorName}</p>
+              </div>
+              <div className="flex items-center">
+                <FontAwesomeIcon
+                  className="mr-1 text-gray-400"
+                  icon={faCalendarDays}
+                />
+                <p className="text-sm text-gray-400">{car.year}</p>
+              </div>
+            </div>
+            <div className="flex justify-between items-center">
+              <p className="font-bold">
+                {car.price}.00$/
+                <span className="text-gray-400 font-bold text-sm">
+                  days
+                </span>{" "}
+              </p>
+              <Link
+                to={`/cardetail/${car.id}`}
+                className="border px-4 py-2 rounded-lg bg-blue-600 text-white font-bold text-sm uppercase"
+              >
+                Rent now
+              </Link>
             </div>
           </div>
-          <div className="flex justify-between items-center">
-            <p className="font-bold">
-              {car.price}.00$/
-              <span className="text-gray-400 font-bold text-sm">days</span>{" "}
-            </p>
-            <Link
-              to={`/cardetail/${car.id}`}
-              className="border px-4 py-2 rounded-lg bg-blue-600 text-white font-bold text-sm uppercase"
-            >
-              Rent now
-            </Link>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
