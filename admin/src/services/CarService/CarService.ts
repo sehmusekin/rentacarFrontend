90b0f110e2e261e3b57c38bb774b0ecb5314741b
Taner <tanerdokmets@gmail.com>
2024-03-02 17:23:11 +0300

    carUpdated added

--- admin/src/services/CarService/CarService.ts
+++ admin/src/services/CarService/CarService.ts
@@ -1,5 +1,6 @@
+import { CarUpdatedModel } from "./../../models/car/CarUpdated";
 import axios from "axios";
-import {Car} from '../../models/car/CarAddModel';
+import { Car } from "../../models/car/CarAddModel";
 
 export default class CarService {
   static async getCars() {
@@ -9,5 +10,7 @@ export default class CarService {
   static async addCar(carData: Car) {
     return axios.post("http://localhost:8080/api/v1/cars/add", carData);
   }
-  
+  static async updateCar(carUpdate: CarUpdatedModel) {
+    return axios.put("http://localhost:8080/api/v1/cars/update", carUpdate);
+  }
 }
