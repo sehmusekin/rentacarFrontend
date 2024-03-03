f41641b44d04da93f2630b1ce9c0f0204e069212
Taner <tanerdokmets@gmail.com>
2024-03-03 14:19:11 +0300

    updated

--- admin/src/components/sidebar/Sidebar.tsx
+++ admin/src/components/sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import NagivationMenu from "../nagivation-menu/NagivationMenu";
 
 const Sidebar = () => {
   return (
-    <div className=" gap-4 w-1/5 p-7 text-black h-screen border-r border-black">
+    <div className=" gap-4 w-1/5 p-7 text-black  border-r border-black">
       <NagivationMenu />
     </div>
   );
